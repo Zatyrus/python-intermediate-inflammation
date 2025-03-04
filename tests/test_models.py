9a273abeb32f89e2edba7f9287145af8240df851
Bela Vogler <62382262+Zatyrus@users.noreply.github.com>
2025-03-04 14:13:17 +0100

    added tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,12 @@
 """Tests for statistics functions within the Model layer."""
 
 import numpy as np
+import pytest
+
+import subprocess
 import numpy.testing as npt
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_mean, daily_min, daily_max, load_csv
 
 
 def test_daily_mean_zeros():
@@ -24,3 +27,51 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+
+def test_daily_min_integers():
+    """Test that min function works for an array of positive integers."""
+
+    test_input = np.array([[1, 2], [3, 4], [5, 6]])
+    test_result = np.array([1, 2])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+def test_daily_max_integers():
+    """Test that max function works for an array of positive integers."""
+
+    test_input = np.array([[1, 2], [3, 4], [5, 6]])
+    test_result = np.array([5, 6])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_string():
+    """Test that min function works for an array of strings."""
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min(
+            np.array([["Hello", "there"], ["General", "Kenobi"]])
+        )
+
+
+# %% Parametrized Tests
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ],
+)
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+    
+# %% Test for exceptions
+def test_load_csv_missing_file():
+    """Test that the load_csv function raises an exception for a missing file."""
+    with pytest.raises(FileNotFoundError):
+        load_csv("data/nonexistent-file.csv")
