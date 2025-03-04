9d3fac63e2c86fb18a78f58cff424187634530f2
Bela Vogler <62382262+Zatyrus@users.noreply.github.com>
2025-03-04 11:48:14 +0100

    ruff format

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,20 +2,20 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
     """
-    return np.loadtxt(fname=filename, delimiter=',')
+    return np.loadtxt(fname=filename, delimiter=",")
 
 
 def daily_mean(data):
@@ -41,4 +41,4 @@ def s_dev(data):
         devs.append((entry - mmm) * (entry - mmm))
 
     s_dev2 = sum(devs) / len(data)
-    return {'standard deviation': s_dev2}
+    return {"standard deviation": s_dev2}
