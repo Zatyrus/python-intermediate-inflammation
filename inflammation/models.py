20d1ec10e64e8d7562017b29f2729a858ba57aad
Bela Vogler <62382262+Zatyrus@users.noreply.github.com>
2025-03-05 13:01:53 +0100

    Update models.py

--- inflammation/models.py
+++ inflammation/models.py
@@ -7,8 +7,10 @@ inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
+import os
 import numpy as np
 
+from typing import List, Dict, Any
 
 def load_csv(filename):
     """Load a Numpy array from a CSV
@@ -50,3 +52,64 @@ def patient_normalise(data):
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
+
+
+### decoumpling task ###
+class Reader:
+    """Creates an instance of the Reader class. This will take a list of file paths on initialization. Use the .read() method to read the files and return the data.
+
+    Raises:
+        ValueError: If the file type is not supported
+        ValueError: If the file path is not valid
+
+    Returns:
+        Reader: Object instance.
+    """
+
+    inpaths: List[str]
+    data_out: Dict[str, Any]
+
+    def __init__(self, inpaths: List[str]) -> "Reader":
+        self.inpaths = inpaths
+        self.data_out = {}
+
+    def read(self, return_data: bool = False, as_list: bool = False):
+        for path in self.inpaths:
+            match path.split(".")[-1]:
+                case "csv":
+                    self.data_out[path] = self.read_csv(path)
+                case "txt":
+                    self.data_out[path] = self.read_txt(path)
+                case "npy":
+                    self.data_out[path] = self.read_npy(path)
+                case _:
+                    raise ValueError(f"File type not supported: {path}")
+
+        if as_list:
+            return list(self.data_out.values())
+        if not return_data:
+            return
+        return self.data_out
+
+    def get_data(self):
+        return self.data_out
+
+    ## Helper
+    def read_csv(self, path: str):
+        return np.loadtxt(fname=path, delimiter=",")
+
+    def read_txt(self, path: str):
+        return np.loadtxt(fname=path)
+
+    def read_npy(self, path: str):
+        return np.load(path)
+
+    ## checkups
+    def check_files_provided(self, inpaths: List[str]):
+        try:
+            assert len(inpaths) > 0
+            assert all([os.path.exists(path) for path in inpaths])
+            assert all([os.path.isfile(path) for path in inpaths])
+
+        except AssertionError:
+            raise ValueError("Please provide a valid file path")
