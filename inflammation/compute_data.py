06687bb0e8633d2f817bf4d250a5ef417c9debf2
Bela Vogler <62382262+Zatyrus@users.noreply.github.com>
2025-03-05 12:58:02 +0100

    abstraction task
    + added Reader class to handle file-read-in
    + added test for Reader class

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -4,7 +4,8 @@ import glob
 import os
 import numpy as np
 
-from inflammation import models, views
+import models
+import views
 
 
 def analyse_data(data_dir):
@@ -14,9 +15,7 @@ def analyse_data(data_dir):
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
     data_file_paths = glob.glob(os.path.join(data_dir, "inflammation*.csv"))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
+    data = models.Reader(data_file_paths).read(return_data=True, as_list=True)
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
@@ -27,3 +26,7 @@ def analyse_data(data_dir):
         "standard deviation by day": daily_standard_deviation,
     }
     views.visualize(graph_data)
+
+
+if __name__ == "__main__":
+    analyse_data("data")
--- tests/test_models.py
+++ tests/test_models.py
@@ -24,3 +24,28 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+
+def test_reader():
+    """Test that Reader class can read in
+    multiple files and return the data."""
+    from inflammation.models import Reader
+
+    test_input = ["data/inflammation-01.csv", "data/inflammation-02.csv"]
+    test_result = {
+        "data/inflammation-01.csv": np.loadtxt(
+            "data/inflammation-01.csv", delimiter=","
+        ),
+        "data/inflammation-02.csv": np.loadtxt(
+            "data/inflammation-02.csv", delimiter=","
+        ),
+    }
+
+    reader = Reader(test_input)
+    reader.read()
+
+    for key, value in reader.data_out.items():
+        npt.assert_array_equal(value, test_result[key])
+
+    for key, known_key in zip(reader.data_out.keys(), test_result.keys()):
+        npt.assert_string_equal(key, known_key)
